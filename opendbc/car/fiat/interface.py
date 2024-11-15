d9799435e1653a19265c062b16543ce2051498ea
Patrick Bassut <patrickbassut@hotmail.com>
2024-11-15 18:44:39 -0300

    things

--- opendbc/car/fiat/interface.py
+++ opendbc/car/fiat/interface.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from panda import Panda
 from opendbc.car import get_safety_config, structs
-from opendbc.car.fiat.values import CAR, RAM_HD, RAM_DT, RAM_CARS, ChryslerFlags
+from opendbc.car.fiat.values import CAR, FastbackFlags
 from opendbc.car.interfaces import CarInterfaceBase
 
 
@@ -17,52 +17,17 @@ class CarInterface(CarInterfaceBase):
 
     # safety config
     ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.chrysler)]
-    if candidate in RAM_HD:
-      ret.safetyConfigs[0].safetyParam |= Panda.FLAG_CHRYSLER_RAM_HD
-    elif candidate in RAM_DT:
-      ret.safetyConfigs[0].safetyParam |= Panda.FLAG_CHRYSLER_RAM_DT
+    ret.safetyConfigs[0].safetyParam |= Panda.FLAG_CHRYSLER_RAM_HD
 
     CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
-    if candidate not in RAM_CARS:
-      # Newer FW versions standard on the following platforms, or flashed by a dealer onto older platforms have a higher minimum steering speed.
-      new_eps_platform = candidate in (CAR.CHRYSLER_PACIFICA_2019_HYBRID, CAR.CHRYSLER_PACIFICA_2020, CAR.JEEP_GRAND_CHEROKEE_2019, CAR.DODGE_DURANGO)
-      new_eps_firmware = any(fw.ecu == 'eps' and fw.fwVersion[:4] >= b"6841" for fw in car_fw)
-      if new_eps_platform or new_eps_firmware:
-        ret.flags |= ChryslerFlags.HIGHER_MIN_STEERING_SPEED.value
 
-    # Chrysler
-    if candidate in (CAR.CHRYSLER_PACIFICA_2018, CAR.CHRYSLER_PACIFICA_2018_HYBRID, CAR.CHRYSLER_PACIFICA_2019_HYBRID,
-                     CAR.CHRYSLER_PACIFICA_2020, CAR.DODGE_DURANGO):
-      ret.lateralTuning.init('pid')
-      ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[9., 20.], [9., 20.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15, 0.30], [0.03, 0.05]]
-      ret.lateralTuning.pid.kf = 0.00006
+    # Newer FW versions standard on the following platforms, or flashed by a dealer onto older platforms have a higher minimum steering speed.
+    new_eps_platform = candidate in (CAR.FASTBACK_LIMITED_EDITION_2024)
+    new_eps_firmware = any(fw.ecu == 'eps' and fw.fwVersion[:4] >= b"6841" for fw in car_fw)
+    if new_eps_platform or new_eps_firmware:
+      ret.flags |= FastbackFlags.HIGHER_MIN_STEERING_SPEED.value
 
-    # Jeep
-    elif candidate in (CAR.JEEP_GRAND_CHEROKEE, CAR.JEEP_GRAND_CHEROKEE_2019):
-      ret.steerActuatorDelay = 0.2
-
-      ret.lateralTuning.init('pid')
-      ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[9., 20.], [9., 20.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15, 0.30], [0.03, 0.05]]
-      ret.lateralTuning.pid.kf = 0.00006
-
-    # Ram
-    elif candidate == CAR.RAM_1500_5TH_GEN:
-      ret.steerActuatorDelay = 0.2
-      ret.wheelbase = 3.88
-      # Older EPS FW allow steer to zero
-      if any(fw.ecu == 'eps' and b"68" < fw.fwVersion[:4] <= b"6831" for fw in car_fw):
-        ret.minSteerSpeed = 0.
-
-    elif candidate == CAR.RAM_HD_5TH_GEN:
-      ret.steerActuatorDelay = 0.2
-      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning, 1.0, False)
-
-    else:
-      raise ValueError(f"Unsupported car: {candidate}")
-
-    if ret.flags & ChryslerFlags.HIGHER_MIN_STEERING_SPEED:
+    if ret.flags & FastbackFlags.HIGHER_MIN_STEERING_SPEED:
       # TODO: allow these cars to steer down to 13 m/s if already engaged.
       # TODO: Durango 2020 may be able to steer to zero once above 38 kph
       ret.minSteerSpeed = 17.5  # m/s 17 on the way up, 13 on the way down once engaged.
