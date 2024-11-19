2e4ff3bf653cf1959ef8c49aacf0c177139eebd0
Patrick Bassut <patrickbassut@hotmail.com>
2024-11-18 23:35:05 -0300

    more dbcs

--- opendbc/car/fiat/carstate.py
+++ opendbc/car/fiat/carstate.py
@@ -23,6 +23,10 @@ class CarState(CarStateBase):
     self.distance_button = 0
 
   def update(self, cp, cp_cam, cp_adas, *_) -> structs.CarState:
+    print(cp.vl)
+    print(cp_cam.vl)
+    print(cp_adas.vl)
+
     ret = structs.CarState()
 
     prev_distance_button = self.distance_button
