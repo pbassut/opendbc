f42319c5e6ddaff6f12d20f5a45ac1e1fb8e7247
Patrick Bassut <patrickbassut@hotmail.com>
2024-11-19 22:15:04 -0300

    more dbcs

--- opendbc/car/fiat/carcontroller.py
+++ opendbc/car/fiat/carcontroller.py
@@ -70,6 +70,7 @@ class CarController(CarControllerBase):
         apply_steer = 0
       self.apply_steer_last = apply_steer
 
+      print('sending lkas command')
       can_sends.append(fiatcan.create_lkas_command(self.packer, self.frame, int(apply_steer)))
 
     self.frame += 1
