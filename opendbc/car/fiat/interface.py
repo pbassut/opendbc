8350bd989ad3b903dbe66e0fb0c3f4a883003a1f
Patrick Bassut <patrickbassut@hotmail.com>
2024-11-25 17:38:39 -0300

    clean up carstate

--- opendbc/car/fiat/interface.py
+++ opendbc/car/fiat/interface.py
@@ -19,17 +19,6 @@ class CarInterface(CarInterfaceBase):
 
     CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
-    # Newer FW versions standard on the following platforms, or flashed by a dealer onto older platforms have a higher minimum steering speed.
-    new_eps_platform = False #candidate in (CAR.FASTBACK_LIMITED_EDITION_2024)
-    new_eps_firmware = any(fw.ecu == 'eps' and fw.fwVersion[:4] >= b"6841" for fw in car_fw)
-    if new_eps_platform or new_eps_firmware:
-      ret.flags |= FastbackFlags.HIGHER_MIN_STEERING_SPEED.value
-
-    #if ret.flags & FastbackFlags.HIGHER_MIN_STEERING_SPEED:
-      # TODO: allow these cars to steer down to 13 m/s if already engaged.
-      # TODO: Durango 2020 may be able to steer to zero once above 38 kph
-      #ret.minSteerSpeed = 17.5  # m/s 17 on the way up, 13 on the way down once engaged.
-
     ret.centerToFront = ret.wheelbase * 0.44
     ret.enableBsm = False
 
