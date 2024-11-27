4ad4457b11378cb43efcb762b1a62ff855433a3b
Patrick Bassut <patrickbassut@hotmail.com>
2024-11-27 18:41:57 -0300

    more dbcs

--- opendbc/car/fiat/carcontroller.py
+++ opendbc/car/fiat/carcontroller.py
@@ -44,7 +44,7 @@ class CarController(CarControllerBase):
     apply_steer = apply_meas_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorqueEps, self.params)
     self.apply_steer_last = apply_steer
 
-    can_sends.append(fiatcan.create_lkas_command(self.packer, self.frame, int(apply_steer)))
+    can_sends.append(fiatcan.create_lkas_command(self.packer, self.frame, apply_steer))
 
     self.frame += 1
 
