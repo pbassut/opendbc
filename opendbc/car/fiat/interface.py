1d870f1692750f058b3ab24195f16a91e5f06dc3
Patrick Bassut <patrickbassut@hotmail.com>
2024-11-27 09:17:45 -0300

    more dbcs

--- opendbc/car/fiat/interface.py
+++ opendbc/car/fiat/interface.py
@@ -15,8 +15,7 @@ class CarInterface(CarInterfaceBase):
     ret.steerLimitTimer = 0.4
 
     # safety config
-    ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.chrysler)]
-    # ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.fcaFastback)]
+    ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.fcaFastback)]
 
     CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
