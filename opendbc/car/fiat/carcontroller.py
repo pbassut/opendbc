f6e110a006093fe7a511c1109f356d0d769ae10b
Patrick Bassut <patrickbassut@hotmail.com>
2025-01-26 19:13:10 -0300

    actuators

--- opendbc/car/fiat/carcontroller.py
+++ opendbc/car/fiat/carcontroller.py
@@ -8,6 +8,8 @@ from opendbc.car.interfaces import CarControllerBase
 class CarController(CarControllerBase):
   def __init__(self, dbc_names, CP):
     super().__init__(dbc_names, CP)
+
+    self.params = CarControllerParams(CP)
     self.apply_steer_last = 0
 
     self.hud_count = 0
@@ -15,7 +17,8 @@ class CarController(CarControllerBase):
     self.last_button_frame = 0
     self.test_counter = 0
 
-    self.packer = CANPacker(dbc_names[Bus.pt])
+    self.packer_pt = CANPacker(dbc_names[Bus.pt])
+    self.packer_adas = CANPacker(dbc_names[Bus.adas])
     self.params = CarControllerParams(CP)
 
   def update(self, CC, CS, now_nanos):
@@ -26,46 +29,46 @@ class CarController(CarControllerBase):
       self.test_counter += 1
 
     # cruise buttons
-    das_bus = 1
-
     # ACC cancellation
     if CC.cruiseControl.cancel:
-      can_sends.append(fiatcan.create_cruise_buttons(self.packer, CS.button_counter + 1, activate=False))
+      can_sends.append(fiatcan.create_cruise_buttons(self.packer_pt, CS.button_counter + 1, activate=False))
 
     # ACC resume from standstill
     elif CC.cruiseControl.resume:
-      can_sends.append(fiatcan.create_cruise_buttons(self.packer, CS.button_counter + 1, activate=True))
+      can_sends.append(fiatcan.create_cruise_buttons(self.packer_pt, CS.button_counter + 1, activate=True))
 
     # longitudinal control
     if self.CP.openpilotLongitudinalControl and CC.longActive:
       # Gas, brakes, and UI commands - all at 100Hz
-      self.apply_gas = int(round(np.interp(actuators.accel)))
-      self.apply_brake = int(round(np.interp(actuators.accel)))
+      self.apply_gas = int(round(interp(actuators.accel)))
+      self.apply_brake = int(round(interp(actuators.accel)))
 
-      can_sends.append(fiatcan.create_gas_command(self.packer, self.apply_gas, CS.accel_counter + 1))
-      can_sends.append(fiatcan.create_friction_brake_command(self.packer, self.apply_brake, CS.accel_counter + 1))
+      can_sends.append(fiatcan.create_gas_command(self.packer_adas, self.apply_gas, CS.accel_counter + 1))
+      can_sends.append(fiatcan.create_friction_brake_command(self.packer_adas, self.apply_brake, CS.accel_counter + 1))
 
     if self.test_counter % 4 == 0:
-      can_sends.append(fiatcan.create_gas_command(self.packer, 12, CS.accel_counter + 1))
+      can_sends.append(fiatcan.create_gas_command(self.packer_adas, 12, CS.accel_counter + 1))
 
     # steering
     # steer torque
     apply_steer = 0
     if CC.latActive:
       new_steer = int(round(actuators.steer * self.params.STEER_MAX))
-      apply_steer = apply_driver_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, self.params) * 0.98
+      apply_steer = apply_meas_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, self.params) * 0.98
 
     self.apply_steer_last = apply_steer
-    can_sends.append(fiatcan.create_lkas_command(self.packer, self.frame, apply_steer, CS.out.vEgo > self.CP.minSteerSpeed))
+    can_sends.append(fiatcan.create_lkas_command(self.packer_pt, self.frame, apply_steer, CS.out.vEgo > self.CP.minSteerSpeed))
 
     if self.frame % 25 == 0:
       eps_faulted = CS.out.steerFaultPermanent or CS.out.steerFaultTemporary
-      can_sends.append(fiatcan.create_lkas_hud_command(self.packer, CC.latActive, eps_faulted))
+      can_sends.append(fiatcan.create_lkas_hud_command(self.packer_pt, CC.latActive, eps_faulted))
 
     self.frame += 1
 
     new_actuators = actuators.as_builder()
     new_actuators.steer = self.apply_steer_last / self.params.STEER_MAX
     new_actuators.steerOutputCan = self.apply_steer_last
+    new_actuators.gas = self.apply_gas
+    new_actuators.brake = self.apply_brake
 
     return new_actuators, can_sends
