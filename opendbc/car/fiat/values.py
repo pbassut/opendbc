bbbd160b5ba41533a12cc40163a1c9cbf7c9ebc9
Patrick Bassut <patrickbassut@hotmail.com>
2025-06-10 22:21:40 -0300

    values

--- opendbc/car/fiat/values.py
+++ opendbc/car/fiat/values.py
@@ -39,8 +39,8 @@ class CarControllerParams:
 
   def __init__(self, CP):
     self.STEER_MAX = 2047
-    self.STEER_DELTA_UP = 7
-    self.STEER_DELTA_DOWN = 10
+    self.STEER_DELTA_UP = 5
+    self.STEER_DELTA_DOWN = 5
     self.STEER_ERROR_MAX = 250
 
     self.STEER_DRIVER_MULTIPLIER = 4  # weight driver torque heavily
