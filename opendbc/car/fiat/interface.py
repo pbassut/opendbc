7687c5396c1c19feb6bcc76a3997976b25c49c64
Patrick Bassut <patrickbassut@hotmail.com>
2025-06-10 22:54:38 -0300

    increase p

--- opendbc/car/fiat/interface.py
+++ opendbc/car/fiat/interface.py
@@ -16,7 +16,7 @@ class CarInterface(CarInterfaceBase):
     ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.fiat)]
 
     CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
-    ret.lateralTuning.torque.kp = 0.10
+    ret.lateralTuning.torque.kp = 0.115
     ret.lateralTuning.torque.ki = 0.37
     ret.lateralTuning.torque.kf = 0.068
 
