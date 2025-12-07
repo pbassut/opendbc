ef4578cfd07baeeab209c6b0d0a3fd6005db675a
Patrick Bassut <patrickbassut@hotmail.com>
2025-12-07 17:17:23 -0300

    alpha lng

--- opendbc/car/tests/routes.py
+++ opendbc/car/tests/routes.py
@@ -17,6 +17,7 @@ from opendbc.car.values import Platform
 from opendbc.car.volkswagen.values import CAR as VOLKSWAGEN
 from opendbc.car.body.values import CAR as COMMA
 from opendbc.car.psa.values import CAR as PSA
+from opendbc.car.fiat.values import CAR as FIAT
 
 # FIXME: add routes for these cars
 non_tested_cars = [
