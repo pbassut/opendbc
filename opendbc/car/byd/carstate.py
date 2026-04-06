f79920b7adc83b0616c6f571bf4bd87527b00a85
Patrick Bassut <patrickbassut@hotmail.com>
2026-04-06 12:24:13 -0300

    updating opendbc

--- opendbc/car/byd/carstate.py
+++ opendbc/car/byd/carstate.py
@@ -1,6 +1,5 @@
-from opendbc.car import Bus, structs
+from opendbc.car import Bus, structs, CANDefine
 from opendbc.can.parser import CANParser
-from opendbc.can.can_define import CANDefine
 from opendbc.car.common.conversions import Conversions as CV
 from opendbc.car.interfaces import CarStateBase
 from opendbc.car.byd.values import DBC, CANBUS, BydSteerStates
