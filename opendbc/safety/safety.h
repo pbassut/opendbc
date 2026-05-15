22479727ad00b5e6f645df780f4c1aef5f90dd3e
Patrick Bassut <patrickbassut@hotmail.com>
2026-05-15 19:38:56 -0300

    endif

--- opendbc/safety/safety.h
+++ opendbc/safety/safety.h
@@ -407,9 +407,10 @@ int set_safety_hooks(uint16_t mode, uint16_t param) {
     {SAFETY_FORD, &ford_hooks},
     {SAFETY_RIVIAN, &rivian_hooks},
     {SAFETY_TESLA, &tesla_hooks},
+    {SAFETY_BYD, &byd_hooks},
 #ifdef CANFD
     {SAFETY_HYUNDAI_CANFD, &hyundai_canfd_hooks},
-    {SAFETY_BYD, &byd_hooks},
+#endif
 #ifdef ALLOW_DEBUG
     {SAFETY_PSA, &psa_hooks},
     {SAFETY_SUBARU_PREGLOBAL, &subaru_preglobal_hooks},
